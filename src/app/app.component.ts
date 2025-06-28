e8ca34298722c862a930096051914e6e12ce8fec
hadeerezzeldin <hadeerezz511@gmail.com>
2025-06-28 17:55:35 +0300

    add nav- home-about-contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,12 @@ import { NavbarsComponent } from '../components/navbars/navbars.component';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from "../components/home/home.component";
 import { MainComponent } from '../components/main/main.component';
+import { FooterComponent } from "../components/footer/footer.component";
+import { SharedNavbarComponent } from '../components/shared-navbar/shared-navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarsComponent, CommonModule, HomeComponent, MainComponent],
+  imports: [RouterOutlet, NavbarsComponent, CommonModule, HomeComponent, MainComponent, RouterOutlet, FooterComponent, SharedNavbarComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -15,7 +17,7 @@ export class AppComponent implements OnInit {
   public title = 'ShippingSystemConsumer';
   isLeftSidebarCollapsed = signal<boolean>(false);
   screenWidth = signal<number>(window.innerWidth);
-marginLeft: any;
+  marginLeft: any;
 
   @HostListener('window:resize')
   onResize() {
@@ -27,6 +29,14 @@ marginLeft: any;
 
   ngOnInit(): void {
     this.isLeftSidebarCollapsed.set(this.screenWidth() < 768);
+    
+    // Hide spinner after page loads
+    setTimeout(() => {
+      const spinner = document.getElementById('spinner');
+      if (spinner) {
+        spinner.classList.remove('show');
+      }
+    }, 1000);
   }
 
   changeIsLeftSidebarCollapsed(isLeftSidebarCollapsed: boolean): void {
